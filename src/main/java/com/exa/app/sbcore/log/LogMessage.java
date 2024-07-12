08bb50c69dbacf958c91b7f5d5883bb3737a66ad
ryvale <valery.nzi@2m2slimited.comm>
2024-07-12 16:04:28 +0000

    Mainteance

--- src/main/java/com/exa/app/sbcore/log/LogMessage.java
+++ src/main/java/com/exa/app/sbcore/log/LogMessage.java
@@ -4,15 +4,17 @@ public class LogMessage {
 	
 	public final static String POTENTIAL_HACK = "POTENTIAL-HACK";
 	
-	public final static String POTENTIAL_HACK_OR_BAD_INIT = "POTENTIAL-HACK/BUG-BAD-INIT";
+	public final static String POTENTIAL_HACK_OR_BAD_INIT = "POTENTIAL-HACK/BAD-INIT";
+	
+	public final static String BAD_INIT = "BAD-INIT";
+	
+	public final static String BAD_CONFIG = "BAD-CONFIG";
+	
+	public final static String CLIENT_ERROR = "CLIENT-ERROR";
 	
 	public final static String BUG_FATAL_ERROR = "BUG-FATAL-ERROR";
 	
-	public final static String BUG_BAD_INIT = "BUG-BAD-INIT";
-	
-	public final static String BUG_BAD_CONFIG = "BUG-BAD-CONFIG";
-	
 	public final static String BUG_MODEL = "BUG-MODEL";
 
-	public final static String BUG_CLIENT = "BUG-CLIENT";
+	
 }
